11bc8d39a7cae2f8ae0bfc3411a2f6d357129daa
Pranav <pranav.techiegeek@gmail.com>
2023-08-28 19:30:10 +0530

    instructor code

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png" />
+
+		<button @click="sendMessage" class="btn btn-info">
+			Send Child A Message
+		</button>
+		<Child :parentmessage="parentmessage" @finished="finished"></Child>
+	</div>
+</template>
+
+<script>
+import Child from "./components/Child.vue";
+
+export default {
+	name: "App",
+	components: {
+		Child,
+	},
+	data: function () {
+		return {
+			parentmessage: "",
+		};
+	},
+	methods: {
+		sendMessage() {
+			this.parentmessage = "<b>Message From Parent:</b> Do Your Homework";
+		},
+		finished() {
+			this.parentmessage = "";
+		},
+	},
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<div
+			v-if="parentmessage"
+			class="card-text alert alert-warning"
+			v-html="parentmessage"
+		></div>
+		<button v-if="parentmessage" @click="ok" class="btn btn-success">
+			Ok
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Child",
+	props: {
+		parentmessage: String,
+	},
+	components: {},
+	methods: {
+		ok() {
+			this.$emit("finished");
+		},
+	},
+};
+</script>
